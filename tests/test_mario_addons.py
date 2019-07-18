0d4bfdf3128f5cb75f2dab64156f03611ecdbcda
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-18 08:52:55 +0000

    Require tests for all included aliases

--- tests/test_mario_addons.py
+++ tests/test_mario_addons.py
@@ -7,18 +7,23 @@ import pytest
 import mario_addons.plugins.addons
 
 
-SPECS = [
-    spec
-    for alias in mario_addons.plugins.addons.registry.aliases.values()
-    for spec in alias.test_specs
-]
+ALIASES = mario_addons.plugins.addons.registry.aliases.values()
+
+SPECS = [spec for alias in ALIASES for spec in alias.test_specs]
 
 
 @pytest.mark.parametrize("test_spec", SPECS)
 def test_alias_test_spec(test_spec: mario.aliasing.AliasTestSpec):
+    """The invocation and input generate the expected output."""
 
     output = subprocess.check_output(
         [sys.executable, "-m", "mario"] + test_spec.invocation,
         input=test_spec.input.encode(),
     ).decode()
     assert output == test_spec.output
+
+
+@pytest.mark.parametrize("alias", ALIASES)
+def test_alias_has_test(alias):
+    """All aliases must have at least one test."""
+    assert alias.test_specs
