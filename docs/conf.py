97a0c7bdfdc56fa2c4832b7f10f36adab546ee1f
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-20 03:41:38 +0000

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = "mario_addons"
 year = "2019"
 author = "Mario contributors"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1.3"
+version = release = "0.1.4"
 
 pygments_style = "trac"
 templates_path = ["."]
