b6926be66df5feaf454120acff01567e830a8357
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-20 04:39:29 +0000

    Use alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ extlinks = {
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only set the theme if we"re building docs locally
-    html_theme = "sphinx_rtd_theme"
+
+html_theme = "alabaster"
 
 html_use_smartypants = True
 html_last_updated_fmt = "%b %d, %Y"
