5ffdc56c9f08b074202237849bf6bbe3b73f8806
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-21 20:53:38 +0000

    Add long-form help and fix some commands

--- tests/test_mario_addons.py
+++ tests/test_mario_addons.py
@@ -2,19 +2,25 @@ import itertools
 import subprocess
 import sys
 
+import attr
 import mario.declarative
 import pytest
 
 import mario_addons.plugins.addons
 
 
-ALIASES = mario_addons.plugins.addons.registry.commands.values()
-
-TEST_SPECS = [test_spec for command in ALIASES for test_spec in command.tests]
+COMMANDS = mario_addons.plugins.addons.registry.commands.values()
+TEST_SPECS = [test_spec for command in COMMANDS for test_spec in command.tests]
 REQUIRED_FIELDS = ["tests", "help", "short_help"]
 
 
-@pytest.mark.parametrize("test_spec", TEST_SPECS)
+def get_param_id(param):
+    if attr.has(type(param)):
+        return repr(attr.asdict(param))
+    return repr(param)
+
+
+@pytest.mark.parametrize("test_spec", TEST_SPECS, ids=get_param_id)
 def test_command_test_spec(test_spec: mario.declarative.CommandTest):
     """The invocation and input generate the expected output."""
 
@@ -25,9 +31,8 @@ def test_command_test_spec(test_spec: mario.declarative.CommandTest):
     assert output == test_spec.output
 
 
-@pytest.mark.parametrize(
-    "command, field_name", itertools.product(ALIASES, REQUIRED_FIELDS)
-)
+@pytest.mark.parametrize("command", COMMANDS, ids=get_param_id)
+@pytest.mark.parametrize("field_name", REQUIRED_FIELDS, ids=get_param_id)
 def test_command_has_required_fields(command, field_name):
     attribute = getattr(command, field_name)
     assert attribute
