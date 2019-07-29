e1c3aa5f2dde8de1ad44178cf789b751548d4bae
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-29 08:10:57 +0000

    Checkout master

--- ci/bump_and_release.bash
+++ ci/bump_and_release.bash
@@ -8,12 +8,19 @@ echo Setup.
 pip install httpie
 pip install towncrier
 
+
+echo Add remote.
+git remote add authorized-origin https://"${GITHUB_TOKEN}"@github.com/"${REPO_OWNER}"/"${REPO_NAME}".git
+
+
 echo Checkout master.
-git checkout master
+git fetch --all
+git checkout -b master authorized-origin/master
 
 
 echo Checking for changelog items.
 python -m towncrier.check || exit 0
+
 
 echo Configure git.
 git config user.name "$GIT_AUTHOR_NAME"
@@ -22,9 +29,6 @@ git config user.email "$GIT_AUTHOR_EMAIL"
 echo Bump version.
 tox -e bump
 
-
-echo Add remote.
-git remote add authorized-origin https://"${GITHUB_TOKEN}"@github.com/"${REPO_OWNER}"/"${REPO_NAME}".git
 
 echo Rebase onto master.
 git fetch authorized-origin/master
